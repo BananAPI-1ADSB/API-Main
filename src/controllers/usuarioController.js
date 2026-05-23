339becc01dc7d1a28756a496f70adb8e3be52374
Gustavo Brun <gustavo.fsantos@sptech.school>
2026-05-23 17:26:19 -0300

    feat: add user authentication

--- src/controllers/usuarioController.js
+++ src/controllers/usuarioController.js
@@ -31,6 +31,48 @@ function cadastrar(req, res) {
   }
 }
 
+function autenticar(req, res) {
+  var email = req.body.emailServer;
+  var senha = req.body.senhaServer;
+
+  if (email == undefined) {
+    res.status(400).send("Seu email está undefined!");
+  } else if (senha == undefined) {
+    res.status(400).send("Sua senha está indefinida!");
+  } else {
+    usuarioModel
+      .autenticar(email, senha)
+      .then(function (resultadoAutenticar) {
+        console.log(`\nResultados encontrados: ${resultadoAutenticar.length}`);
+        console.log(`Resultados: ${JSON.stringify(resultadoAutenticar)}`);
+
+        if (resultadoAutenticar.length == 1) {
+          console.log(resultadoAutenticar);
+
+          res.json({
+            id: resultadoAutenticar[0].id,
+            nome: resultadoAutenticar[0].nome,
+            email: resultadoAutenticar[0].email,
+            fkEmpresa: resultadoAutenticar[0].fkEmpresa,
+          });
+        } else if (resultadoAutenticar.length == 0) {
+          res.status(403).send("Email e/ou senha inválido(s)");
+        } else {
+          res.status(403).send("Mais de um usuário com o mesmo login e senha!");
+        }
+      })
+      .catch(function (erro) {
+        console.log(erro);
+        console.log(
+          "\nHouve um erro ao realizar o login! Erro: ",
+          erro.sqlMessage,
+        );
+        res.status(500).json(erro.sqlMessage);
+      });
+  }
+}
+
 module.exports = {
   cadastrar,
+  autenticar,
 };
