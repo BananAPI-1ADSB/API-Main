66b35e895d75d7d80351b37b02fa3f42e9fa5382
Gustavo Brun <gustavo.fsantos@sptech.school>
2026-06-06 13:52:23 -0300

    fix: fix controller import

--- src/routes/enderecos.js
+++ src/routes/enderecos.js
@@ -1,10 +1,10 @@
 var express = require("express");
 var router = express.Router();
 
-var enderecosController = require("../controllers/enderecosController");
+var enderecoController = require("../controllers/enderecoController");
 
 router.post("/cadastrar", function (req, res) {
-  enderecosController.cadastrar(req, res);
+  enderecoController.cadastrar(req, res);
 });
 
 module.exports = router;
